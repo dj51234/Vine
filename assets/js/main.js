6c8865c25726cfa6da61897a3cf33eddf04e3a12
Derrick <dj51234@yahoo.com>
2015-08-20 00:50:24 -0400

    about and people

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,4 +3,11 @@ $(document).ready(function() {
 		$('.nav-side-wrap').toggleClass('open');
 		return false;
 	});
+
+	$('.face').mouseenter(function(){
+		$(this).siblings('.info').children('p').css({'opacity':'1','transform':'rotate(0deg)'});
+	});
+	$('.face').mouseleave(function(){
+		$(this).siblings('.info').children('p').css({'opacity':'0','transform':'rotate(25deg)'});;
+	});
 });
